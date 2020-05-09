f88f7690963cb7de04dbd2a91a359373803f893b
kkmitra <kaushik.mitra@innoraft.com>
2020-05-09 20:29:21 +0530

    Updated the css
    - Change the book listing page UI to look like popular books section in
    the home page
    - Added styles to the register page.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,65 +5,65 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="page__header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}" class="form">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="form__group row">
+                            <label for="name" class="col-md-4 form__label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form__input @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="name...">
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="error-message" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form__group row">
+                            <label for="email" class="col-md-4 form__label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form__input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="email...">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="error-message" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form__group row">
+                            <label for="password" class="col-md-4 form__label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form__input @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="enter your password...">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="error-message" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="form__group row">
+                            <label for="password-confirm" class="col-md-4 form__label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form__input" name="password_confirmation" required autocomplete="new-password" placeholder="please type your passwrord again...">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form__group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="button button--primary">
                                     {{ __('Register') }}
                                 </button>
                             </div>
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,25 +3,39 @@
 @section('content')
 <div class="container">
   <div class="page__header">List of books</div>
-  <ul class="list">
-    @foreach($books as $book)
-      <li class="list-item">
+  <ul class="list list--grid">
+    @forelse($books as $book)
+    <li class="list-item">
+      <div class="list-item__image">
+        <img src="https://source.unsplash.com/random/300x100" alt="list-image"
+          class="image">
+      </div>
+      <div class="list-item__group">
         <h1 class="list-item__title">
-          <a class="list-item__link" href="{{ route('book', $book->id)}}">{{ $book->name }}</a>
+          <a class="list-item__link"
+            href="{{ route('book', $book->id)}}">{{ $book->name }}</a>
         </h1>
-        <div class="list-item__body">
-          <p>Quantity left {{ $book->quantity }}</p>
-          <a class="button button--secondary" href="{{ route('order_book', $book->id) }}"
-            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+        <div class="list-item__image-container">
+          <a class="list-item__button list-item__button--secondary"
+            href="{{ route('order_book', $book->id) }}"
+            onclick="event.preventDefault();document.getElementById('order-book').submit();">
             {{ __('Order Now') }}
           </a>
+          <form id="order-book" action="{{ route('order_book', $book->id) }}"
+            method="POST" class="u-display-none;">
+            @csrf
+          </form>
         </div>
-
-        <form id="logout-form" action="{{ route('order_book', $book->id) }}" method="POST" class="u-display-none;">
-          @csrf
-        </form>
-      </li>
-    @endforeach
+      </div>
+      <p class="list-item__body">
+        <!-- {{ $book->synopsis }} -->
+        {{ \Illuminate\Support\Str::limit($book->synopsis, 100, $end='...') }}
+      </p>
+      <p class="list-item__footer">{{ $book->author_name }}</p>
+    </li>
+    @empty
+    <p>Nothing to show...</p>
+    @endforelse
   </ul>
 </div>
 @endsection
